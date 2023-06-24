b96967a79ae64214d8452360e5ab17378189b1ca
Pamela544 <112887738+Pamela544@users.noreply.github.com>
2023-06-23 23:29:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let numero = 0;  // Se utiliza para saber el nº de pokemon a adivinar
+let cantidadAcertados = 0;  // Se utiliza para saber cuantos pokemons fueron adivinados
+let cantidadEncuestados = 0;  // Se utiliza para saber cuantos pokemons se intentaron adivinar
+
+// Alumno: crear la función restart:
+
+// 1) Debe borrar el contenido de texto de "resultText"
+// 2) Debe quitar la clase "success" de la imagen "imgpokemon"
+// 3) Debe crear un número aleatorio de 0 a la cantidad de pokemons
+// disponibles en la variable "pokemons" (proveniente del archivo data.js)
+// Esa variable la debe guardar en "numero".
+// 5) Debe modificar el atributo de "src" de "imgpokemon" para
+// que muestre como imagen el "thumbnail" del nº de pokemon a adivinar
+// (utilice la variable "numero" para buscar el pokemon candidato en "pokemons")
+
+// function restart
+
+
+agregarEventos();
+restart();
+
+
+
+// Función restart
+function restart() {
+  // Borrar el contenido de texto de "resultText"
+  document.getElementById("resultText").textContent = "";
+
+  // Quitar la clase "success" de la imagen "imgpokemon"
+  document.getElementById("imgpokemon").classList.remove("success");
+
+  // Generar número aleatorio de 0 a la cantidad de pokemons disponibles
+  numero = Math.floor(Math.random() * pokemons.length);
+
+  // Modificar el atributo "src" de "imgpokemon" con el "thumbnail" del Pokémon candidato
+  document.getElementById("imgpokemon").src = pokemons[numero].thumbnail;
+}
+
+
+
